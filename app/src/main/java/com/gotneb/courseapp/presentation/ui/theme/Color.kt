579db39c6a629fe74a0f94f68d7da17f8e0f1560
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2025-01-03 17:26:03 -0400

    Update Color.kt

--- app/src/main/java/com/gotneb/courseapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/gotneb/courseapp/presentation/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Collor pallete
+val roxoClaro = Color(0xFF774DED)
+val roxo = Color(0xFF33357B)
+val roxoEscuro = Color(0xFF202244)
+val cinza = Color(0xFF333333)
+val brancoFosco = Color(0xFFF4F5FE)
+val brancoPastel = Color(0xFFBBBBBB)
